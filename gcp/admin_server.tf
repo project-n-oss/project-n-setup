2042eebab53447256490c8fcb91596fe067a5267
Jacob Donenfeld <jd@projectn.co>
2022-02-14 18:53:45 +0000

    Call update before installing projectn package

--- gcp/admin_server.tf
+++ gcp/admin_server.tf
@@ -13,8 +13,8 @@ chown -R ${local.ssh_username} /home/${local.ssh_username} 2>> $log
 echo $(ls -la /home/${local.ssh_username}) >> $log
 echo $(ls -la /home/${local.ssh_username}/.project-n) >> $log
 echo '{"default_platform":"gcp"}' > /home/${local.ssh_username}/.project-n/config 2>> $log
-sudo yum -y install ${var.package_url} 2>> $log
 sudo yum -y update
+sudo yum -y install ${var.package_url} 2>> $log
   EOF
   # labels, metadata, resource_policies, and tags are all set automatically, and may cause the server to be recreated.
   labels            = {}
