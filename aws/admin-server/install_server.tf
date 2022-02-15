cc58c5dafefb765f9ac84b6fc5726d04a3cfde57
Jacob Donenfeld <jd@projectn.co>
2022-02-15 19:25:52 +0000

    (SUP-4794): Add sudo yum update to admin server install

--- aws/admin-server/install_server.tf
+++ aws/admin-server/install_server.tf
@@ -57,6 +57,7 @@ resource "aws_instance" "admin" {
   }
   user_data                   = <<EOF
 #!/bin/bash
+yum -y update
 yum -y install ${var.package_url}
 su ec2-user -c 'pip3 install awscli awscli-plugin-bolt --user'
 echo 'export PATH=~/.local/bin:$PATH' >> /home/ec2-user/.bash_profile && chown ec2-user /home/ec2-user/.bash_profile
