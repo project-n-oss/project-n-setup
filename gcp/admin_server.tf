c7556b2e47f18d339a340d33285bf83518d31045
Kote <km@projectn.co>
2024-02-29 19:24:50 -0700

    feat: aws and gcp init scripts -x

--- gcp/admin_server.tf
+++ gcp/admin_server.tf
@@ -16,6 +16,7 @@ resource "google_compute_instance" "admin" {
   machine_type            = var.admin_server_instance_type
   metadata_startup_script = <<EOF
 #!/bin/bash
+set -x
 mkdir -p /home/${local.ssh_username}/.project-n
 log="/home/${local.ssh_username}/.setup-log"
 echo "=== Setup log ===" > $log
